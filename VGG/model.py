ed202feb1de8402242f87f8e49072cf6d32c040c
ZHAO Yi-heng <zyh2399701602@gmail.com>
2025-08-31 14:30:38 +0800

    Add files via upload

---
+++ VGG/model.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+
+class VGG16(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(VGG16, self).__init__()
+
+        self.block1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+        
+        self.block2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+        
+        self.block3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+        
+        self.block4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+        
+        self.block5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(7*7*512, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes))
+        
+        # 权重初始化
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+
+        return x
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+
---
+++ VGG/test.py
@@ -0,0 +1,62 @@
+import torch
+from torchvision.datasets import FashionMNIST
+import torch.nn as nn
+from torchvision import transforms
+import torch.utils.data as Data
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+from model import VGG16
+
+def test_data_process(batch_size=128):
+    test_data = FashionMNIST(root="./data",
+                                  train=False,
+                                  transform = transforms.Compose([transforms.Resize(size=224),transforms.ToTensor()]),
+                                  download=True)
+    test_dataloader = Data.DataLoader(dataset=test_data,
+                                     batch_size=batch_size,
+                                     shuffle=False,
+                                     num_workers=4)  
+    return test_dataloader
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("device:{}".format(device))
+
+def test_process(model, test_dataloader):
+
+    model = model.to(device)
+
+
+    # 初始化参数
+    test_corrects = 0
+    test_num = 0
+
+    model.eval()
+    for batch_idx, (inputs, labels) in enumerate(test_dataloader):
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        # 将数据放入设备
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        with torch.no_grad():
+
+            # 前向传播
+            outputs = model(inputs)
+
+            # 统计信息
+
+            test_num += labels.size(0)
+            pre_lab = torch.argmax(outputs, dim=1)
+            test_corrects += (pre_lab == labels).sum().item()
+
+    test_acc = test_corrects / test_num
+    print("测试准确率为：{:.4f}".format(test_acc))
+
+
+if __name__ == "__main__":
+    model = VGG16().to(device)
+    model.load_state_dict(torch.load('best_model.pth'))
+    test_data = test_data_process()
+    test_process = test_process(model, test_data)
+
+
